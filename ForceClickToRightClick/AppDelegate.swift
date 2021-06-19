1e32fde933e84c49937092c4a6cd4b818bd15582
Jed Fox <git@jedfox.com>
2021-06-19 14:19:57 -0400

    Document Unmanaged usage

--- ForceClickToRightClick/AppDelegate.swift
+++ ForceClickToRightClick/AppDelegate.swift
@@ -59,6 +59,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let event = NSEvent(cgEvent: cgEvent),
            let wrapper = ctx?.load(as: Wrapper.self) {
           if let newEvent = handle(event: event, cgEvent: cgEvent, wrapper: wrapper, proxy: proxy) {
+            /// Quoting from https://developer.apple.com/documentation/coregraphics/cgeventtapcallback?language=swift
+            /// Your callback function should return one of the following:
+            /// - The (possibly modified) event that is passed in. This event is passed back to the event system.
+            ///   - [we call passUnretained here since the event system is retaining the original event]
+            /// - A newly-constructed event. After the new event has been passed back to the event system, the new event will be released along with the original event.
+            ///   - [we call passRetained here because the event system will eventually release the event we return]
+            /// - `NULL` if the event passed in is to be deleted.
             if newEvent == cgEvent {
               return .passUnretained(cgEvent)
             } else {
